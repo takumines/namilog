dffa29bc8584e21a261c7a723336e763bfa3a7da
takumi_kurogi <takumines1@gmail.com>
2020-08-20 22:58:58 +0900

    [update]電話番号のカラムを追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('phone_number')->nullable();
             $table->integer('stance')->default(1);
             $table->integer('board')->default(1);
             $table->string('introduction')->nullable();
