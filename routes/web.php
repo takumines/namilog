0f374bd6bf87a002f826a124c534c9ab5829d1ff
takumi_kurogi <takumines1@gmail.com>
2020-08-20 23:51:33 +0900

    [refactor]ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,24 @@
 */
 Route::get('/', 'StartController@home')->name('start.home');
 
-/* Auth */
+/*
+ * Auth
+ */
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function()
-{
-  /* spot */
+Route::group(['middleware' => 'auth'], function() {
+
+  /*
+   * spot
+   */
   Route::get('/spot/create', 'SpotController@create')->name('spot.create');
   Route::post('/spot/create', 'SpotController@store');
   Route::get('/spot/edit/{spot}', 'SpotController@edit')->name('spot.edit');
   Route::post('/spot/edit/{spot}', 'SpotController@update');
   Route::get('/spot/{spot}', 'SpotController@show')->name('spot.show');
-  /* diary */
+  /*
+   * diary
+   */
   Route::get('/diary/list', 'Diarycontroller@list')->name('diary.list');
   Route::get('/diary/create', 'DiaryController@create')->name('diary.create');
   Route::post('/diary/create', 'DiaryController@store');
@@ -31,16 +37,24 @@ Route::group(['middleware' => 'auth'], function()
   Route::get('/diary/edit/{diary}', 'DiaryController@edit')->name('diary.edit');
   Route::post('/diary/edit/{diary}', 'DiaryController@update');
   Route::post('/diary/del/{diary}', 'DiaryController@delete')->name('diary.delete');
-  /* user */
+  /*
+   * user
+   */
   Route::get('/user/list', 'UserController@list')->name('user.list');
-  Route::get('/user/edit/{id}', 'UserController@edit')->name('user.edit');
-  Route::post('/user/edit/{id}', 'UserController@update');
-  Route::get('/user/{id}', 'UserController@show')->name('user.show');
-  /* comments */
+  Route::get('/user/{user}', 'UserController@show')->name('user.show');
+  Route::get('/user/edit/{user}', 'UserController@edit')->name('user.edit');
+  Route::post('/user/edit/{user}', 'UserController@update');
+
+  /*
+   * comment
+   */
   Route::post('/comment', 'CommentController@create')->name('comment.create');
   Route::post('/comment/del', 'CommentController@delete')->name('comment.delete');
+
 });
 
-/* twitter */
+/*
+ * twitter
+ */
 Route::get('login/twitter', 'Auth\LoginController@redirectToTwitterProvider')->name('twitter.login');
 Route::get('login/twitter/callback', 'Auth\LoginController@handleTwitterProviderCallback');
